3e620794284e3b3d7d08a757dceea76030837d0b
Anamika-s <anamika.sawhney22@gmail.com>
2024-01-08 13:54:58 +0530

    added changes

--- ArrayDemo/BatchProgram.cs
+++ ArrayDemo/BatchProgram.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +15,42 @@ namespace ArrayDemo
             //batch.Get();
             //batch.Display();
 
-            Batch[] batches= new Batch[10];
-            for (int i = 0; i < 3; i++)
+            //Batch[] batches= new Batch[10];
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    batches[i] = new Batch();
+            //    batches[i].Get();
+            //}
+
+            //foreach (Batch batch in batches)
+            //{   if(batch!=null)
+            //    batch.Display();
+            //}
+
+            List<Batch> batches= new List<Batch>();
+            string ch = "y";
+            while(ch != "y")
             {
-                batches[i] = new Batch();
-                batches[i].Get();
+                
+                Batch batch = new Batch();
+                batch.Get();
+                batches.Add(batch);
+                Console.WriteLine("Add more");
+                ch = Console.ReadLine();
             }
 
-            foreach (Batch batch in batches)
-            {   if(batch!=null)
-                batch.Display();
-            }
+            int[] marks = new int[10];
+            int[] marks1 = new int[] { 1,2,3,4 };
+            // Initialize COllection
+            // Collection Initalizer
+
+            //List<Batch> batchList = new List<Batch>()
+            //{
+            //    new Batch(
+            //}
+
+
+
 
             }
 
--- ArrayDemo/GenericCollectionDemo.cs
+++ ArrayDemo/GenericCollectionDemo.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic;  // this namesnspace is used for generic collectio
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +8,21 @@ namespace ArrayDemo
 {
     internal class GenericCollectionDemo
     {
+        static void Main()
+        {
+            // ArrayList > List
+            List<int> list = new List<int>();
+            list.Add(1);
+            //list.Add("aaaa");
+
+            Queue<string> list2 = new Queue<string>();
+
+            Stack<string> stack = new Stack<string>();
+
+            Dictionary<int, string> list1 = new Dictionary<int, string>(); 
+
+
+
+        }
     }
 }
